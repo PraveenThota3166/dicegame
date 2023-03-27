ba4af6a832cc372472d9a2f7ba4108542cf4c346
PraveenThota3166 <120163414+PraveenThota3166@users.noreply.github.com>
2023-03-27 12:26:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1=Math.random();
+randomNumber1=randomNumber1*6;
+randomNumber1=Math.floor(randomNumber1)+1;
+var randomImg1="images/dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImg1);
+var randomNumber2=Math.random();
+randomNumber2=randomNumber2*6;
+randomNumber2=Math.floor(randomNumber2)+1;
+var randomImg2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImg2);
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player1 Wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player2 Wins";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
